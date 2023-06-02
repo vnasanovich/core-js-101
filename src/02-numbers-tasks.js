316ea45428c50674bf6bc7f55d0ed3d927a3e401
Valentina Nasanovich <valentina.nasanovich@gmail.com>
2023-06-02 22:30:39 +0300

    complete some numbers, object and date tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -109,8 +109,9 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  return Math.acos(x1 * x2 + y1 * y2)
+ / (Math.sqrt(x1 ** 2 + y1 ** 2) * Math.sqrt(x2 ** 2 + y2 ** 2));
 }
 
 /**
@@ -201,8 +202,11 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
+    if (n % i === 0) return false;
+  }
+  return n > 1;
 }
 
 /**
